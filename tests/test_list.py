7cbd946de61bf35de33d5aec7a11b25c499edb80
Sergei <s.vasilchenko@busfor.com>
2020-01-14 16:08:38 +0300

    add some changes

--- tests/test_list.py
+++ tests/test_list.py
@@ -1,11 +1,7 @@
 import pytest
-import allure
-def test_sun():
-    assert 2+3 == 5
 
-def test_division():
-    #assert 2/0 == 1
-    with pytest.raises(ZeroDivisionError):
-        print(4/0)
 
+def test_one():
+    mobile_vendors = ['apple', 'xiaomi', 'alcatel', 'samsung', 'xiaomi']
+    print(mobile_vendors.reverse())
 
