dee3d9734eef43008017bce244d72686e89dbc54
Sergei <s.vasilchenko@busfor.com>
2020-01-18 01:02:04 +0300

    add fixes for docstrings

--- tests/test_string.py
+++ tests/test_string.py
@@ -16,20 +16,20 @@ def test_string_replace(setup_string_example):
 
 @pytest.mark.string
 def test_string_len(setup_string_example):
-    """Проверяем, что длину строку"""
+    """Проверяем длину строки"""
     assert len(setup_string_example) == 11
 
 
 @pytest.mark.string
 def test_string_concatenation(setup_string_example):
-    """Проверяем, что множество включает элементы, а значит ненулевое"""
+    """Проверяем, что после конкатенации получилась новая строка"""
     second_string = "blablabla"
     assert setup_string_example + second_string == "abracadabrablablabla"
 
 
-@pytest.mark.parametrize('setup_string_example', [("bra")])
+#@pytest.mark.parametrize('setup_string_example', [("bra")])
 @pytest.mark.string
 def test_string_count(setup_string_example):
-    """Проверяем, что в множествах есть пересекающиеся элементы"""
+    """Проверяем, количество вхождений в получаемой строке"""
     print(setup_string_example)
-    assert setup_string_example.count("abra") == 1
+    assert setup_string_example.count("abra") == 2
